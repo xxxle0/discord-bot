ec1d2fac5cd5a0a0e6b338b34108032fe22422cb
duytq <duybkit13@gmail.com>
2019-09-07 09:10:42 +0700

    add dotenv for token discord bot

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const clientDiscord = require('./discord.singleton').client;
+require('dotenv').config();
 
-const TOKEN_DISCORD = "NjE5NDg3NjI4NjQ2OTQwNjc0.XXI9TA.6WTLv8u8_hUW467CuagSTLBsmNU"
-
+const TOKEN_DISCORD = process.env.TOKEN_DISCORD
+console.log(TOKEN_DISCORD);
 clientDiscord.on("ready", () => {
     console.log('logged in as ' + clientDiscord.user.tag);
 });
 
 clientDiscord.on("message", msg => {
-    console.log(msg);
     if (msg.content === '<@561598381416251407>') {
         msg.reply('Đợi xíu bố mày đang gọi nó');
     }
